cf420c39cdeffcaf4c1a7ba06c518b88f26345c3
dudaGrossi <dcoutogrossi@gmail.com>
2024-11-27 19:58:52 -0300

    Estruturação da parte de Admin

--- loja/admin.py
+++ loja/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Cliente
+from .models import Produto
+from .models import Pedido
+from .models import Avaliacao
+from .models import Carrinho
+from .models import Pagamento
+from .models import Estoque
+from .models import PedidoProduto
+from .models import ProdutoCarrinho
 
-# Register your models here.
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nome", "email")
+    search_fields = ["nome"]
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "codigo", "preco")
+    search_fields = ["nome", "codigo"]
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ("cliente", "valor", "status")
+
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ("autor", "pedido")
+
+#carrinho não está atualizando, verificar
+class CarrinhoAdmin(admin.ModelAdmin):
+    list_display = ("cliente", Produto)
+
+class PagamentoAdmin(admin.ModelAdmin):
+    list_display = ("pedido", "metodo", "data")
+
+class EstoqueAdmin(admin.ModelAdmin):
+    list_display = ("produto", "quantidade")
+
+class PedidoProdutoAdmin(admin.ModelAdmin):
+    list_display = ("pedido", "produto", "quantidade")
+
+class ProdutoCarrinhoAdmin(admin.ModelAdmin):
+    list_display = ("produto", "quantidade")
+
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Pedido, PedidoAdmin)
+admin.site.register(Avaliacao, AvaliacaoAdmin)
+admin.site.register(Carrinho, CarrinhoAdmin)
+admin.site.register(Pagamento, PagamentoAdmin)
+admin.site.register(Estoque, EstoqueAdmin)
+admin.site.register(PedidoProduto, PedidoProdutoAdmin)
+admin.site.register(ProdutoCarrinho, ProdutoCarrinhoAdmin)
+
+#login e senha: admin
--- loja/models.py
+++ loja/models.py
@@ -7,13 +7,18 @@ class Cliente(models.Model):
     email = models.EmailField(unique = True)
     telefone = models.CharField(max_length=15)
     dataNasc = models.DateField()
-    
+
+    def __str__(self):
+        return self.nome
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
     codigo = models.CharField(max_length=5, unique = True)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits = 10, decimal_places = 2)
+
+    def __str__(self):
+        return self.nome
 
 class Pedido(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE, related_name = "pedidos")
@@ -22,22 +27,34 @@ class Pedido(models.Model):
     data = models.DateField(auto_now_add = True)
     status = models.CharField(max_length=10,choices = [('Pendente', 'Pendente'), ('Pago', 'Pago'), ('Enviado', 'Enviado'), ('Entregue', 'Entregue')])
 
+    def __str__(self):
+        return self.cliente.nome
+
 class Avaliacao(models.Model):
     autor = models.ForeignKey(Cliente, on_delete = models.CASCADE, related_name = "avaliacoes")
     pedido = models.ForeignKey(Pedido, on_delete = models.CASCADE, related_name = "avaliacoes")
     comentario = models.TextField(blank = True, null = True)
     estrelas = models.IntegerField(choices = [(1, '1 estrela'), (2, '2 estrela'), (3, '3 estrela'), (4, '4 estrela'), (5, '5 estrela')])
 
+    def __str__(self):
+        return self.autor.nome
+
 
 class Carrinho(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE, related_name = "carrinhos")
     produto = models.ManyToManyField(Produto, through = 'ProdutoCarrinho', related_name = "carrinhos")
+
+    def __str__(self):
+        return self.cliente.nome
 
 
 class Pagamento(models.Model):
     pedido = models.OneToOneField(Pedido, on_delete=models.CASCADE, related_name="pagamento")
     metodo = models.CharField(max_length= 10, choices = [('Cartão', 'Cartão'), ('Boleto', 'Boleto'), ('Pix', 'Pix')])
     data = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.pedido.cliente.nome
 
 class Estoque(models.Model):
     produto = models.ForeignKey(Produto, on_delete = models.CASCADE, related_name = "estoque")
